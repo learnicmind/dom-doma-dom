b9c29b1098a71e8e33690c8386e8f2ddc3a2fe6f
Ariful Islam Razu <learnicmind@gmail.com>
2023-02-09 16:32:49 +0600

    dom2

---
+++ index.js
@@ -0,0 +1,10 @@
+// getElement by id
+const myDom = document.getElementById('dom');
+myDom.innerHTML = 'Hello Bangladesh';
+
+const myPara = document.getElementById('para');
+myPara.innerHTML = "hello i am not only a paragraph i am also a dom";
+
+
+const paraTwo = document.getElementsByClassName('para-2');
+paraTwo.innerHTML = 'this is paragraph two';
